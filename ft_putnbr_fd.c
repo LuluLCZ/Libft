e054c3b377a654eb14bb7c8a5edc9580c6f3c90b
LuluLCZ <lacaze.lulu@gmail.com>
2017-09-19 19:02:05 +0200

    ft_fd

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Lulu <Lulu@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/09/19 18:40:46 by Lulu              #+#    #+#             */
+/*   Updated: 2017/09/19 19:01:50 by Lulu             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void    ft_putchar_fd(char c, int fd)
+{
+    write(fd, &c, 1);
+}
+
+void    ft_putstr_fd(const char *s, int fd)
+{
+    int     i;
+
+    i = 0;
+    while (s[i])
+    {
+        ft_putchar_fd(s[i], fd);
+        i++;
+    }
+}
+
+void    ft_putnbr_fd(int n, int fd)
+{
+    if (n == -2147483648)
+        ft_putstr_fd("-2147483648");
+    else
+    {
+        if (n < 0)
+        {
+            n = -n;
+            ft_putchar_fd('-', fd);
+        }
+        else if (n > 9)
+            ft_putnbr_fd(n / 10);
+        ft_putchar_fd((nb % 10) + '0', fd);
+    }
+}
