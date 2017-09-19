31e2de94e7701062319f05b4dbdcba9eebcf884a
LuluLCZ <lacaze.lulu@gmail.com>
2017-09-19 10:41:14 +0200

    mem

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -1,19 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: Lulu <Lulu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/09/14 13:41:14 by Lulu              #+#    #+#             */
-/*   Updated: 2017/09/18 12:06:35 by Lulu             ###   ########.fr       */
+/*   Created: 2017/09/18 12:23:04 by Lulu              #+#    #+#             */
+/*   Updated: 2017/09/18 12:26:30 by Lulu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
-#include <stdio.h>
 
-size_t  ft_strlcat(char *dst, const char *src, size_t size)
+int     ft_memcmp(const void *s1, const void *s2, size_t n)
 {
+    size_t  i;
     
+    i = 0;
+    while (i < n)
+    {
+        if (s1[i] != s2[i])
+            return (s1[i] - s2[i])
+        i++;
+    }
+    return (0);
 }
--- ft_memmove.c
+++ ft_memmove.c
@@ -1,19 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: Lulu <Lulu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/09/14 13:41:14 by Lulu              #+#    #+#             */
-/*   Updated: 2017/09/18 12:06:35 by Lulu             ###   ########.fr       */
+/*   Created: 2017/09/18 12:29:47 by Lulu              #+#    #+#             */
+/*   Updated: 2017/09/18 13:10:21 by Lulu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
+#include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-size_t  ft_strlcat(char *dst, const char *src, size_t size)
+void    *ft_memmove(void *dst, const void *src, size_t len)
 {
-    
+    char *str;
+    size_t n;
+
+    n = 0;
+    str = dst;
+    while (n < len)
+    {
+        
+    }
 }
